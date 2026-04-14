d2bcb78cab3044aca5de296ab7a7c6455c55f6f0
takagino <hirotaka.takagi@trident.ac.jp>
2026-04-15 08:02:56 +0900

    0415

--- 01/sketch.js
+++ 01/sketch.js
@@ -1,15 +1,9 @@
-let myBox; // 「myBox」という名前の箱（変数）を用意します
-
 function setup() {
-  new Canvas('16:9');
-
-  // スプライト（キャラクター）を新しく作って、myBoxに入れる
-  myBox = new Sprite(); // プロパティを変更する（. を使う）
-  myBox.width = 100; // 幅を100に
-  myBox.height = 150; // 高さを150に
-  myBox.color = 'red'; // 色を赤に（文字は '' で囲むルール）
+  new Canvas(640, 480);
 }
 
 function draw() {
-  background(220);
+  console.log(frameCount);
+
+  background('black');
 }
